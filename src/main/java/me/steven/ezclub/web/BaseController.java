a86da80f9baacdf7157c3e74e7172ebd4520e9ad
Steven <stevensonghc@gmail.com>
2018-11-09 00:44:22 +0800

    change some service domains; half done registration function.

--- src/main/java/me/steven/ezclub/web/BaseController.java
+++ src/main/java/me/steven/ezclub/web/BaseController.java
@@ -1,14 +1,14 @@
 package me.steven.ezclub.web;
 
 import me.steven.ezclub.dto.ResponseData;
+import me.steven.ezclub.dto.parameter.UserRegistration;
 import me.steven.ezclub.service.CityService;
 import me.steven.ezclub.service.CollegeService;
 import me.steven.ezclub.service.ProvinceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("")
 public class BaseController {
@@ -36,6 +36,15 @@ public class BaseController {
         ResponseData result = new ResponseData();
         result.setMsg("BE server connected");
         result.setData(pvSvs.listProvinces());
+        return result;
+    }
+
+    @PostMapping("submitRegister")
+    public ResponseData submitRegister(@RequestBody UserRegistration registration) {
+        ResponseData result = new ResponseData();
+        System.out.println("email " + registration.getEmail());
+        System.out.println("nickname " + registration.getNickname());
+        System.out.println("password " + registration.getPassword());
         return result;
     }
 
